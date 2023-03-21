339e3c9e0ab93780fac7a1f1f863979ad30b2504
Jessica Hattig <jessicahattig@jessicas-mbp.lan>
2023-03-21 10:53:39 -0700

    added function that converts gallons to liters.

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 // buisness logic
-function milliliters(tsp,tbsp,cups) {
-  return tsp * (4.929) + tbsp * (14.787) + cups * (236.6)
+function liters(gallons){
+  return gallons * (3.785)
 }
 
 // user interface logic
-const tsp = parseInt(prompt("Enter teaspoons:"));
-const tbsp = parseInt(prompt("Enter tablespoons:"));
-const cups = parseInt(prompt("Enter cups:"));
+const gallons = parseInt(prompt("Enter amount of gallons to convert to liters:"));
 
-window.alert(milliliters(tsp,tbsp,cups));
+window.alert(liters(gallons));
